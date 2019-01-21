f7a78b9e74bb9a67f1ddd14c747353574799a11a
boudlal <boudlal98@gmail.com>
2019-01-21 10:39:05 +0000

    add repositories List Component + get and map data from github api

--- src/components/RepositoriesListComponent.vue
+++ src/components/RepositoriesListComponent.vue
@@ -1,23 +1,114 @@
 <template>
-  <div class="content">
-
+  <div class="container">
+    <div class="container-title">
+      <h2>Most strarred repositories</h2>
+    </div>
+    <div class="row">
+    	<div v-for="repo in repositories" class="card col-md-12 p-3">
+    	 <div class="row ">
+    			<div class="col-md-3">
+    				<img class="w-100" :src="repo.user_avatar">
+    			</div>
+    		<div class="col-md-8">
+    			<div class="card-block">
+    					<a :href="repo.url"><h3 class="card-title">{{ repo.title }}</h3></a>
+    					<p class="card-text text-justify">{{ repo.description }}</p>
+    					<a href="https://www.google.com" class="btn btn-default">Stars: {{ repo.stars_num }}</a>
+    					<a href="https://www.google.com" class="btn btn-default">Issues: {{ repo.issues_num}}</a>
+              <span>Submitted {{ repo.created_at }} days ago by {{ repo.user_name}}</span>
+    			</div>
+    		</div>
+    		</div>
+    	</div>
+    </div>
+    <pulse-loader class="loader" v-show="loading"></pulse-loader>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+
+import axios from 'axios'
+import PulseLoader  from 'vue-spinner/src/PulseLoader.vue'
+
 export default {
+  components: {
+    PulseLoader
+  },
   data () {
     return {
-
+      loading: true
     }
+  },
+  computed: {
+    repositories () {
+      return this.$store.getters.repositories
+    }
+  },
+  created () {
+    this.$store.dispatch('getRepositories', this.pageNumber).then(() => {
+      this.loading = false;
+    })
+
   }
 }
 </script>
 
+
 <style scoped>
-.content {
-  min-height: 600px
+.container {
+  min-height: 500px
 }
 
-.img{}
+.container-title {
+  text-align: left;
+  margin: 5px;
+  padding: 10px
+}
+
+img {
+  width: 60%;
+}
+
+.row {
+  margin: 0px;
+}
+
+.card-title {
+  text-align: left;
+  text-transform: uppercase;
+	color: #E44424;
+}
+
+.card {
+  border-radius:0;
+  margin:10px auto;
+}
+
+.card-block {
+  text-align: left;
+}
+
+.card-text {
+	margin-top:10px;
+	margin-bottom: 10px;
+	background-color:#FFFFFF;
+	color:#000000;
+}
+
+a.btn, a.btn:visited {
+  color:#333333;
+}
+
+hr {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  border: 0;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+.loader {
+  margin: 10px
+}
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,43 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import moment from 'moment'
 
 Vue.use(Vuex)
+Vue.use(axios)
 
 export const store = new Vuex.Store({
   state: {
-
+    repositories: []
   },
   getters: {
-
+    repositories (state) {
+      return state.repositories.map((x) => {
+        return {
+          title: x.name.substring(0, 30),
+          url: x.html_url,
+          description: x.description,
+          stars_num: x.stargazers_count,
+          issues_num: x.open_issues,
+          user_name: x.owner.login,
+          user_avatar: x.owner.avatar_url,
+          created_at: moment().diff(moment(x.created_at), 'days')
+        }
+      })
+    }
   },
   mutations: {
-
+    getRepositories (state, payload) {
+      this.state.repositories = this.state.repositories.concat(payload)
+      return this.state.repositories
+    }
   },
   actions: {
-
+    getRepositories (context) {
+      return axios.get(`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc`)
+      .then((res) => {
+        context.commit('getRepositories', res.data.items)
+      })
+    }
   }
 })
